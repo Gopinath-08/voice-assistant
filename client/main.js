9611dea07854e8fd7d8f740edb3e544b9295daad
Gopinath-08 <105555567+Gopinath-08@users.noreply.github.com>
2023-03-13 11:50:08 +0530

    Update main.js

--- client/main.js
+++ client/main.js
@@ -9,7 +9,7 @@ const recognition = new SpeechRecognition();
 recognition.interimResults = false;
 
 const getAnswear = async (question) => {
-  const response = await fetch("https://voice-mate.onrender.com", {
+  const response = await fetch("https://voice-assistant.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
